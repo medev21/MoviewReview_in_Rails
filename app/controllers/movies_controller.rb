7342ce71d9c331fcd38a73d07e6e3b0413701c05
Martin Ben <mebenav88@gmail.com>
2015-12-26 20:22:41 -0600

    add movie show edit update destroy controller, add movie show view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
 
   def index
     @movies = Movie.all.order('created_at DESC')
@@ -17,9 +18,26 @@ class MoviesController < ApplicationController
       render 'new'
     end
   end
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 
   private
     def movie_params
       params.require(:movie).permit(:title, :description, :director)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:id])
     end
 end
