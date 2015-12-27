ad939ca7758bca8afbe99b96957e873071cfc979
Martin Ben <mebenav88@gmail.com>
2015-12-26 20:04:01 -0600

    add movie new create controller, movie new form views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,23 @@ class MoviesController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :description, :director)
+    end
 end
