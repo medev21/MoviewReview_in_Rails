a9ecae5a98b205c460cd6b1ee5ed607e08d873c8
Martin Ben <mebenav88@gmail.com>
2015-12-28 21:54:54 -0600

    add paperclip gem and its reqs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228051001) do
+ActiveRecord::Schema.define(version: 20151229035336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,14 @@ ActiveRecord::Schema.define(version: 20151228051001) do
     t.string   "title"
     t.text     "description"
     t.string   "director"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
     t.integer  "category_id"
+    t.string   "movie_img_file_name"
+    t.string   "movie_img_content_type"
+    t.integer  "movie_img_file_size"
+    t.datetime "movie_img_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
